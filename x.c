9e754b80aa2fd79f1abee05f306a8d800ba3b7cd
Mohsin Kaleem <mohkalsin@gmail.com>
2020-03-15 21:16:07 +0000

    apply patch: st-clipboard-0.8.2.diff

--- x.c
+++ x.c
@@ -661,6 +661,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+    clipcopy(NULL);
 }
 
 void
@@ -682,7 +684,7 @@ brelease(XEvent *e)
 	if (e->xbutton.button == Button1)
 		mousesel(e, 1);
     else if (e->xbutton.button == Button3)
-        selpaste(NULL);
+        clippaste(NULL);
 }
 
 void
