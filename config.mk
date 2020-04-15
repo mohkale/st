77f4dacd77a13a4c4c3e3a628f46e4f8800ca20b
mohkale <mohkale@kisara.moe>
2020-04-16 00:25:39 +0100

    makefile: make install directory configurable
    by default `make install` now installs to the users local (home)
    directory instead of globally. Why? this doesn't require sudo
    permissions.

--- config.mk
+++ config.mk
@@ -4,8 +4,13 @@ VERSION = 0.8.2
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+# PREFIX = /usr/local
+# DESKTOP_DIR = /usr/share/applications
+# MANPREFIX = $(PREFIX)/share/man
+
+PREFIX = $(HOME)/.local
+DESKTOP_DIR = $(HOME)/.local/share/applications
+MANPREFIX = $(HOME)/.local/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
